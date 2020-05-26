e874419576dc58c4d3a9836001afd37e2570f700
Viviane Miranda <vivianehfm@gmail.com>
2020-05-26 16:00:44 -0300

    excs 1 e 2 e 3 resolvidos

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,14 +1,36 @@
 // map(): faz uma iteração com todos os elementos da lista e retorna outra
 // 1) Adicionar "a" no final das palavras
 const profissionais = ["desenvolvedor", "programador", "autor", "vereador", "professor"];
+const profissionaisMelhores = profissionais.map(
+  (item) => {
+    return item + "a";
+  }
+);
+// console.log (profissionaisMelhores)
 
 // 2) Retornar frutas com somente a primeira letra maiúscula
 const frutas = ["maçã", "banana", "Pera", "Melancia", "pêsSSEgO", "jAcA"];
+
+const frutasMaiuscula = frutas.map (
+  (item) => {
+  const novaFruta = item.toLowerCase();
+  return novaFruta.charAt(0).toUpperCase() + novaFruta.slice(1);
+  }
+)
+// console.log (frutasMaiuscula)
 
 
 // filter(): retorna uma nova array com apenas com elementos que atendem à condição
 // 1) Criar nova array somente com números pares
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const numPares = num.filter (
+ (item) => {
+   const numPar = item%2 === 0;
+   return numPar
+ } 
+)
+// console.log(numPares)
 
 // 2) Criar nova array somente com profissionais da área de TI
 const pessoas = [
@@ -19,6 +41,14 @@ const pessoas = [
   { nome: "Monica", area: "medicina", profissao: "cardiologista"},
 ];
 
+const pessoasTI = pessoas.filter(
+  (item) => {
+    let pessoaTI = item.area === 'TI'
+    return pessoaTI
+  }
+)
+
+console.log (pessoasTI)
 
 // reduce(): recebe uma array e retorna um valor só. Redutor.
 // 1) Dado uma array de números, retornar a soma de todos eles
